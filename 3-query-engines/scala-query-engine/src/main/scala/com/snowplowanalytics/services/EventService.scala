992fec1dc3895a3602ff5349faf99083e80220df
bigsnarfdude <ohprecio@gmail.com>
2015-07-10 14:40:55 -0700

    working group return

--- 3-query-engines/scala-query-engine/src/main/scala/com/snowplowanalytics/services/EventService.scala
+++ 3-query-engines/scala-query-engine/src/main/scala/com/snowplowanalytics/services/EventService.scala
@@ -19,6 +19,7 @@ import java.util.TimeZone
 import java.text.SimpleDateFormat
 
 import scala.collection.mutable.ArrayBuffer
+import scala.collection.parallel.mutable
 
 //import com.amazonaws.services.dynamodbv2.model.{ScanRequest, ProjectionType, KeyType}
 //import com.sun.xml.internal.bind.v2.schemagen.xmlschema.AttributeType
@@ -123,12 +124,7 @@ object EventService {
     val bucket = "2015-06-05T12:56:00.000"
     val timestampResult: Seq[awscala.dynamodbv2.Item] = table.scan(Seq("Timestamp" -> cond.eq(bucket)))
     val attribsOfFourElements: Seq[Seq[awscala.dynamodbv2.Attribute]] = timestampResult.map(_.attributes)
-
-
-    convertDataStage1(attribsOfFourElements)
-
-
-    List(SimpleEvent(Some(123), Some("asdfasdf"), Some("asdfasdf"), Some(123)))
+    convertDataStage1(attribsOfFourElements).toList
   }
 
   // provide "Red", "2015-06-20T12:32:00.00" and get count back
@@ -150,12 +146,16 @@ object EventService {
     timestampResult.flatMap(_.attributes.find(_.name == "EventType").map(_.value.s.get))
   }
 
+
   // convertDataStage1(attribsOfFourElements)
-  def convertDataStage1(myArray: Seq[Seq[awscala.dynamodbv2.Attribute]]) {
-     for (a <- myArray) {
-       val result = a.map(unpack)
-       //createSimpleEvents(result)
-     }
+  def convertDataStage1(myArray: Seq[Seq[awscala.dynamodbv2.Attribute]]): scala.collection.mutable.ArrayBuffer[com.snowplowanalytics.model.SimpleEvent] =  {
+    var myList = scala.collection.mutable.ArrayBuffer.empty[SimpleEvent]
+    for (a <- myArray) {
+      val result = a.map(unpack)
+      println(result)
+      myList += SimpleEvent(Some(result(0).toInt),Some(result(1)),Some(result(3)),Some(10))
+    }
+    myList
   }
 
 
@@ -169,8 +169,8 @@ object EventService {
   def unpack(x: Any): String = x match {
     case Attribute("Count", value) => value.getN
     case Attribute("EventType", value) => value.getS
-    case Attribute("CreatedAt", value) => value.getN
     case Attribute("Timestamp", value) => value.getS
+    case Attribute("CreatedAt", value) => value.getN
   }
 
 
