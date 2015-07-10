bdc26306652a2b9711405050bad5518e7dc59a17
bigsnarfdude <ohprecio@gmail.com>
2015-07-10 15:56:52 -0700

    clean up

--- 3-query-engines/scala-query-engine/src/main/scala/com/snowplowanalytics/services/EventService.scala
+++ 3-query-engines/scala-query-engine/src/main/scala/com/snowplowanalytics/services/EventService.scala
@@ -18,6 +18,8 @@ import java.util.Date
 import java.util.TimeZone
 import java.text.SimpleDateFormat
 
+import com.snowplowanalytics.services.EventData._
+
 import scala.collection.mutable.ArrayBuffer
 import scala.collection.parallel.mutable
 
@@ -89,42 +91,21 @@ object EventService {
     dateFormatter.format(new Date())
   }
 
-
-  //////////////////////////////////////////////////////////////
-  // temp data
-  //////////////////////////////////////////////////////////////
-
-  import EventData.testEvents
-
-  def getEvents2: List[SimpleEvent] = {
-    testEvents.toList
-  }
-
-  def getEventById(eventId: Long): Option[SimpleEvent] = {
-    testEvents find (_.id == Some(eventId))
-  }
-
-  def getEvents3: List[SimpleEvent] = {
+  def getEventsFilterGreen: List[SimpleEvent] = {
     testEvents.toList.filter(x => x.eventType == "Green")
   }
 
-
-
-
-
   // provide "Red", "2015-06-20T12:32:00.00" and get count back
   def getEvents5(): List[SimpleEvent] = {
-
-      println(getCountByEventTypeTimestamp(eventType, timestamp, table))
+    println(getCountByEventTypeTimestamp(eventType, timestamp, table))
     List(SimpleEvent(Some(123), Some("asdfasdf"), Some("asdfasdf"), Some(123)))
   }
 
   def getEvents(): List[SimpleEvent] = {
-
     val bucket = "2015-06-05T12:56:00.000"
     val timestampResult: Seq[awscala.dynamodbv2.Item] = table.scan(Seq("Timestamp" -> cond.eq(bucket)))
     val attribsOfFourElements: Seq[Seq[awscala.dynamodbv2.Attribute]] = timestampResult.map(_.attributes)
-    convertDataStage1(attribsOfFourElements).toList
+    convertDataStage(attribsOfFourElements).toList
   }
 
   // provide "Red", "2015-06-20T12:32:00.00" and get count back
@@ -146,26 +127,16 @@ object EventService {
     timestampResult.flatMap(_.attributes.find(_.name == "EventType").map(_.value.s.get))
   }
 
-
-  // convertDataStage1(attribsOfFourElements)
-  def convertDataStage1(myArray: Seq[Seq[awscala.dynamodbv2.Attribute]]): scala.collection.mutable.ArrayBuffer[com.snowplowanalytics.model.SimpleEvent] =  {
+  def convertDataStage(myArray: Seq[Seq[awscala.dynamodbv2.Attribute]]): scala.collection.mutable.ArrayBuffer[com.snowplowanalytics.model.SimpleEvent] =  {
     var myList = scala.collection.mutable.ArrayBuffer.empty[SimpleEvent]
     for (a <- myArray) {
       val result = a.map(unpack)
       println(result)
-      myList += SimpleEvent(Some(result(0).toInt),Some(result(1)),Some(result(3)),Some(10))
+      myList += SimpleEvent(Some(result(0).toInt),Some(result(1)),Some(result(3)),Some(result(0).toInt))
     }
     myList
   }
 
-
-  //def createSimpleEvents(x:Any): SimpleEvent = {
-    //SimpleEvent(Some(123), Some(x(1).toString), Some(x(0).toString), Some(123))
-  //}
-
-
-
-  // attribsOfFourElements.map(_.foreach(unpack))
   def unpack(x: Any): String = x match {
     case Attribute("Count", value) => value.getN
     case Attribute("EventType", value) => value.getS
@@ -173,8 +144,17 @@ object EventService {
     case Attribute("CreatedAt", value) => value.getN
   }
 
+  //////////////////////////////////////////////////////////////
+  // temp data and methods below
+  //////////////////////////////////////////////////////////////
 
+  def getEvents2: List[SimpleEvent] = {
+    testEvents.toList
+  }
 
+  def getEventById(eventId: Long): Option[SimpleEvent] = {
+    testEvents find (_.id == Some(eventId))
+  }
 
   def addEvent(event: SimpleEvent): SimpleEvent = {
     val maxId = testEvents.map(_.id).flatten.max + 1
