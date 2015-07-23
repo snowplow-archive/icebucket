e1f1c9e86fdb09c6a04d6357f269b1e48d0a8744
bigsnarfdude <ohprecio@gmail.com>
2015-07-22 17:52:25 -0700

    add models

---
+++ 3-query-engines/scala-query-engine/src/main/scala/com/snowplowanalytics/model/DruidResponse.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2015 Snowplow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package com.snowplowanalytics.model
+
+import com.snowplowanalytics.model.DruidRequestJsonProtocol._
+import spray.json._
+import spray.httpx.SprayJsonSupport
+import spray.json.DefaultJsonProtocol
+
+case class DruidResponse(timestamp: String, result: List[com.snowplowanalytics.model.SimpleEvent])
+
+/**"
+ * Implements spray-json support so DruidResponse case class can be marshalled
+ * to/from json when accepting and completing requests
+ *
+ *
+ *   [
+ *    {
+ *      "timestamp": "2012-01-01T00:00:00.000Z",
+ *      "result": { "Green": 23, "Red": 12, "Blue": 14 }
+ *    },
+ *    {
+ *      "timestamp": "2012-01-02T00:00:00.000Z",
+ *      "result": { "Purple": 17, "Red": 120, "Yellow": 4 }
+ *    }
+ *  ]
+ *
+ *
+ */
+object DruidResponseJsonProtocol extends DefaultJsonProtocol with SprayJsonSupport {
+  implicit val druidResponseFormat = jsonFormat4(DruidRequest)
+}
